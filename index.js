3e5f97fa3c6a1cf7643ca3c336ae4d8bb300e1df
ahmed yasser <ahmedya3ser8@gmail.com>
2025-05-31 15:06:59 +0300

    handle not found route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import employeeRouter from './routes/employee.route.js';
+import { ERROR } from './utils/httpStatus.js';
 
 const app = express();
 
@@ -18,7 +19,12 @@ mongoose.connect(process.env.MONGO_URL)
   process.exit(1);
 })
 
-app.use('/api/employees', employeeRouter)
+app.use('/api/employees', employeeRouter);
+
+// Handle 404 - Route not found
+app.use((req, res) => {
+  res.status(404).json({ status: ERROR, message: 'Resource not found', path: req.originalUrl });
+});
 
 app.listen(process.env.PORT, '127.0.0.1', () => {
   console.log('server running http://localhost:8000');
